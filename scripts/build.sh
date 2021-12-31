0c4c137e551f69035e10093ced1af63878404b66
asigaru123 <two.626f6f6b@gmail.com>
2021-12-31 12:02:31 +0900

    codedeploy周りの設定ファイルを変更

--- scripts/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd /home/ec2-user
-cp /home/ec2-user/springboot/target/Tweet-App-0.0.1-SNAPSHOT.jar docker/app/
-docker-compose build
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd /home/ec2-user
-docker-compose up -d
+docker-compose up -d --build
